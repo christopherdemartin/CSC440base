197c018706b68daefc254731ea3e425d9c4b6298
Victoria Iacono-Henderson <victoriaiaconohend@mail.adelphi.edu>
2015-12-07 10:24:36 -0500

    Sprint 2
    Updated GUI

---
+++ Warehouse/Database.java
@@ -0,0 +1,23 @@
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Database {
+    public static String db() {
+        Connection conn = null;
+        Statement st = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println("Connection to driver done");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "Matchman11");
+            st = conn.createStatement();
+            st.execute("insert into sakila.actor (first_name, last_name) values ('kris', 'pepper'); ");
+            return "good";
+        }
+        catch (Exception ex) {
+            System.out.println("sqlException: " + ex.getMessage());
+            return "bad";
+        }
+    }
+}
